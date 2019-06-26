5e6d18b17cd5b3455b0889230f359e94aec7c4c1
Paul Tobin <tobster111@gmail.com>
2019-06-26 17:04:01 +0100

    Using asynchronous + non-blocking fs

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,11 +1,28 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-console.log(textIn);
+// console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 
-fs.writeFileSync('./txt/output.txt', textOut);
+// fs.writeFileSync('./txt/output.txt', textOut);
 
-console.log("File has been written!")
+// console.log('File has been written!');
+
+// Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+        console.log('Your file has been written');
+      });
+    });
+  });
+});
+
+console.log('Reading file...');
