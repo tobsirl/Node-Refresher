38fce828e6bfeff514f81da2a1deae9c22c4531d
Paul Tobin <tobster111@gmail.com>
2019-06-27 19:04:01 +0100

    template-overview is displaying the data correctly

--- node-farm/index.js
+++ node-farm/index.js
@@ -37,6 +37,20 @@ const url = require('url');
 ///////////////////////////////////////////////////////////////////////////////////////////
 // !Server
 ///////////////////////////////////////////////////////////////////////////////////////////
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -59,7 +73,13 @@ const server = http.createServer((req, res) => {
   // overview page
   if (pathName === '/' || pathName === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
-    res.end(tempOverview);
+
+    const cardsHtml = dataObject
+      .map(card => replaceTemplate(tempCard, card))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
     // product page
   } else if (pathName === '/product') {
     res.end('Product Page');
