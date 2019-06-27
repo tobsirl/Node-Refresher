1e15ff8ac0ba1370f2b1cdc97c1df3dbd85323c2
Paul Tobin <tobster111@gmail.com>
2019-06-27 18:05:10 +0100

    Using readFileSync to call each of the templates

--- node-farm/index.js
+++ node-farm/index.js
@@ -38,19 +38,36 @@ const url = require('url');
 // !Server
 ///////////////////////////////////////////////////////////////////////////////////////////
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end('Overview Page');
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.end(tempOverview);
+    // product page
   } else if (pathName === '/product') {
     res.end('Product Page');
+    // API
   } else if (pathName === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
+    // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html'
