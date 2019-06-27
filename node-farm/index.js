e3e2b09bc0c105ffbc8c208eb612172ee7fe80e7
Paul Tobin <tobster111@gmail.com>
2019-06-27 16:28:03 +0100

    Create /api endpoint, read in the data from the json file

--- node-farm/index.js
+++ node-farm/index.js
@@ -47,6 +47,12 @@ const server = http.createServer((req, res) => {
     res.end('Overview Page');
   } else if (pathName === '/product') {
     res.end('Product Page');
+  } else if (pathName === '/api') {
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+      const productData = JSON.parse(data);
+      res.writeHead(200, {'Content-type': 'application/json'})
+      res.end(data);
+    });
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html'
