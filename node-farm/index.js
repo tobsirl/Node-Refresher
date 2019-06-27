68cab7a55f555b12d872375bc955913b7367d29e
Paul Tobin <tobster111@gmail.com>
2019-06-27 16:35:33 +0100

    Using readFileSync due to the data being read once

--- node-farm/index.js
+++ node-farm/index.js
@@ -38,9 +38,10 @@ const url = require('url');
 // !Server
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
 
+const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === '/' || pathName === '/overview') {
@@ -48,11 +49,8 @@ const server = http.createServer((req, res) => {
   } else if (pathName === '/product') {
     res.end('Product Page');
   } else if (pathName === '/api') {
-    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, {'Content-type': 'application/json'})
-      res.end(data);
-    });
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html'
