e98d9debc3294fe37a4957461a8b3d771b20de77
Paul Tobin <tobster111@gmail.com>
2019-06-29 19:30:17 +0100

    Creating a sales class that extends EventEmitter

--- how-node-works/events.js
+++ how-node-works/events.js
@@ -1,17 +1,29 @@
 const EventEmitter = require('events');
+const http = require('http');
 
-const myEmitter = new EventEmitter();
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sales();
 
 myEmitter.on('newSale', () => {
   console.log('There was a new sale!');
 });
 
-myEmitter.on('newSale', () => {
-  console.log('Costumer name: Paul');
+myEmitter.on('newSale', name => {
+  console.log(`Costumer name: ${name.name}`);
 });
 
 myEmitter.on('newSale', stock => {
-  console.log(`There are now ${stock} items left in stock.`);
+  console.log(`There are now ${stock.items} items left in stock.`);
 });
 
-myEmitter.emit('newSale', 9);
+const stockItems = {
+  items: 9,
+  name: 'Paul'
+};
+
+myEmitter.emit('newSale', stockItems);
