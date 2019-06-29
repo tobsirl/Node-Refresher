36984b3392aa54a86f2b9bbf5204366ffdee302f
Paul Tobin <tobster111@gmail.com>
2019-06-29 16:45:44 +0100

    Using process.nextTick() which gets executed after each phase

--- how-node-works/event-loop.js
+++ how-node-works/event-loop.js
@@ -5,6 +5,13 @@ setImmediate(() => console.log('Immediate 1 finished'));
 
 fs.readFile('text-file.txt', () => {
   console.log('I/O finished');
+  console.log('-------------------------------------------');
+
+  setTimeout(() => console.log('Timer 2 finished'), 0);
+  setTimeout(() => console.log('Timer 3 finished'), 3000);
+  setImmediate(() => console.log('Immediate 2 finished'));
+
+  process.nextTick(() => console.log('Process.nextTick'));
 });
 
 console.log('Hello from the top-level code');
