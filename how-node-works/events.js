1c2b2794802a28ef484bc84efabc9c74c5951101
Paul Tobin <tobster111@gmail.com>
2019-06-29 18:41:40 +0100

    You can pass data with the emitter

--- how-node-works/events.js
+++ how-node-works/events.js
@@ -10,4 +10,8 @@ myEmitter.on('newSale', () => {
   console.log('Costumer name: Paul');
 });
 
-myEmitter.emit('newSale');
+myEmitter.on('newSale', stock => {
+  console.log(`There are now ${stock} items left in stock.`);
+});
+
+myEmitter.emit('newSale', 9);
