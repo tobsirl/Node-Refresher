aae626db00e215044fc2e8d3a7d18415366e8818
Paul Tobin <tobster111@gmail.com>
2019-06-29 18:05:58 +0100

    Using the Sync version of pbkdf2 function

--- how-node-works/event-loop.js
+++ how-node-works/event-loop.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 1;
 
 setTimeout(() => console.log('Timer 1 finished'), 0);
 setImmediate(() => console.log('Immediate 1 finished'));
@@ -16,21 +17,17 @@ fs.readFile('text-file.txt', () => {
 
   process.nextTick(() => console.log('Process.nextTick'));
 
-  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-    console.log(Date.now() - start, 'Password encrypted');
-  });
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
 
-  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-    console.log(Date.now() - start, 'Password encrypted');
-  });
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
 
-  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-    console.log(Date.now() - start, 'Password encrypted');
-  });
-  
-  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-    console.log(Date.now() - start, 'Password encrypted');
-  });
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
+
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
 });
 
 console.log('Hello from the top-level code');
