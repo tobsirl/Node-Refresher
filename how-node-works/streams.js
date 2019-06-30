5df880ef9ed6b48877744a9a5078187908e1e6d1
Paul Tobin <tobster111@gmail.com>
2019-06-30 09:34:09 +0100

    Streams - streaming the data chunk by chunk

--- how-node-works/streams.js
+++ how-node-works/streams.js
@@ -3,9 +3,25 @@ const server = require('http').createServer();
 
 server.on('request', (req, res) => {
   // solution 1
-  fs.readFile('test-file.txt', (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  // fs.readFile('test-file.txt', (err, data) => {
+  //   if (err) console.log(err);
+  //   res.end(data);
+  // });
+
+  // solution 2 - streams
+  const readable = fs.createReadStream('test-file.txt');
+  readable.on('data', chunk => {
+    res.write(chunk);
+  });
+
+  readable.on('end', () => {
+    res.end();
+  });
+
+  readable.on('error', err => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end('File not found!');
   });
 });
 
