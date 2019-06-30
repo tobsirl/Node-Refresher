483481e72b0809df630fe935b46e81b8a3bca0b4
Paul Tobin <tobster111@gmail.com>
2019-06-30 13:08:25 +0100

    Example has three nested callback functions

--- asynchronous-JS/index.js
+++ asynchronous-JS/index.js
@@ -7,6 +7,11 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
-      console.log(res.body);
+      if (err) return console.log(err.message);
+      console.log(res.body.message);
+      fs.writeFile('dog-image.txt', res.body.message, err => {
+        if (err) return console.log(err.message);
+        console.log('Random dog image saved to file!');
+      });
     });
 });
