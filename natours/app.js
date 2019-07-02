e458c0890cb164d2ee3bd6ee8c9574c357aa6181
Paul Tobin <tobster111@gmail.com>
2019-07-02 18:59:05 +0100

    Added a middleware function and calling next

--- natours/app.js
+++ natours/app.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
