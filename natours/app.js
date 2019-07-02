faa7a58fb86654e4a620b25c786f0416f1723cae
Paul Tobin <tobster111@gmail.com>
2019-07-02 17:50:25 +0100

    Finding a specific record in the json file

--- natours/app.js
+++ natours/app.js
@@ -18,8 +18,18 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const tour = tours.find(el => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
