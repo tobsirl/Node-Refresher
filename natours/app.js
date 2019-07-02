3c3924d973aa53d52807c09b28adfc7d26e4afab
Paul Tobin <tobster111@gmail.com>
2019-07-02 19:11:33 +0100

    Used a third party middleware

--- natours/app.js
+++ natours/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const fs = require('fs');
 
+const morgan = require('morgan');
+
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
   next();
 });
 
@@ -15,8 +23,10 @@ const tours = JSON.parse(
 );
 
 app.get('/api/v1/tours', (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestAt: req.requestTime,
     data: {
       tours: tours
     }
