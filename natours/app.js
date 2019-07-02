bdecd8581a0f8d61cac50441e649a1cd0d3ce1be
Paul Tobin <tobster111@gmail.com>
2019-07-02 15:48:21 +0100

    Read in the data using readFileSync and send the data back on the new endpoint

--- natours/app.js
+++ natours/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
 app.get('/api/v1/tours', (req, res) => {
-  
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: tours
+    }
+  });
 });
 
 const port = 3000;
