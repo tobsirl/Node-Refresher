ed460ffe08fecf03d60b3851897b510230733a83
Paul Tobin <tobster111@gmail.com>
2019-07-02 17:55:28 +0100

    Return a 404 if the id isnt found

--- natours/app.js
+++ natours/app.js
@@ -21,6 +21,13 @@ app.get('/api/v1/tours', (req, res) => {
 app.get('/api/v1/tours/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const tour = tours.find(el => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
