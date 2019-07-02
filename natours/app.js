c1da2e6ffb39eef167dfe056426849e8b3ec2a67
Paul Tobin <tobster111@gmail.com>
2019-07-02 16:10:56 +0100

    Using a post request to send data and save it to the json file

--- natours/app.js
+++ natours/app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -14,6 +16,25 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours
     }
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour
+        }
+      });
+    }
+  );
 });
 
 const port = 3000;
