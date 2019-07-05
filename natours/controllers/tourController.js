d43f44abede0edfe3c481f01bccb2726ca8c8803
Paul Tobin <tobster111@gmail.com>
2019-07-05 16:42:41 +0100

    Implemented get all tours

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,17 +1,21 @@
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
-  
-
-
-  const tours = await Tour.find();
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours
-    }
-  });
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.getTour = (req, res) => {
