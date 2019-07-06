87f32389ebed847d478a52e441e7678bf0ce0f08
Paul Tobin <tobster111@gmail.com>
2019-07-06 09:52:10 +0100

    Excluding fields such as page, sort, limit and fields

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -2,14 +2,13 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
     // Two different methods of filtering, objects vs built in mongoose
 
-    const tours = await Tour.find({
-      duration: 5,
-      difficulty: 'easy'
-    });
+    const tours = await Tour.find(queryObj);
 
     // const tours = await Tour.find()
     //   .where('duration')
