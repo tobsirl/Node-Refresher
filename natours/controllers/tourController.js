adfb6612ac6b31bf69fae64ca89f124587c6ef17
Paul Tobin <tobster111@gmail.com>
2019-07-06 09:31:55 +0100

    Using query to filter the results from an api call

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -2,7 +2,21 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    console.log(req.query);
+
+    // Two different methods of filtering, objects vs built in mongoose
+
+    const tours = await Tour.find({
+      duration: 5,
+      difficulty: 'easy'
+    });
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
