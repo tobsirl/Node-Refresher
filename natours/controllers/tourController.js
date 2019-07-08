f1f40631710113be67983db255290512e8c687cd
Paul Tobin <tobster111@gmail.com>
2019-07-08 17:37:59 +0100

    Implementing the sorting on get all tours

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -3,18 +3,23 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // Build the query
-    // Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    // Advanced Filtering
+    // 1B) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
     // Two different methods of filtering, objects vs built in mongoose
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      query = query.sort(req.query.sort);
+    }
 
     // const tours = Tour.find()
     //   .where('duration')
