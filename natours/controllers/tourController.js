80e6f2b0dc4844a4bb674b7831cd7baacd8cef9b
Paul Tobin <tobster111@gmail.com>
2019-07-09 20:08:40 +0100

    Implemented the paginate() method and returned each object with this, allow us to chain the methods

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -38,13 +38,24 @@ class APIFeatures {
     return this;
   }
 
-  limitField() {
+  limitFields() {
     if (this.queryString.fields) {
       const fields = this.queryString.fields.split(',').join(' ');
       this.query = this.query.select(fields);
     } else {
       this.query = this.query.select('-__v');
     }
+    return this;
+  }
+
+  paginatate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10, 1-10, page 1, 11-20 page 2
+    this.query = this.query.skip(skip).limit(limit);
+
     return this;
   }
 }
@@ -69,7 +80,7 @@ exports.getAllTours = async (req, res) => {
     // if (req.query.sort) {
     //   const sortBy = req.query.sort.split(',').join(' ');
     //   query = query.sort(sortBy);
-    //   // sort('price ratingsAverage)
+    // sort('price ratingsAverage)
     // } else {
     //   query = query.sort('-createdAt');
     // }
@@ -89,20 +100,23 @@ exports.getAllTours = async (req, res) => {
     // }
 
     // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
 
     // page=2&limit=10, 1-10, page 1, 11-20 page 2
-    query = query.skip(skip).limit(limit);
+    // query = query.skip(skip).limit(limit);
 
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw Error('This page does not exist');
-    }
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments();
+    //   if (skip >= numTours) throw Error('This page does not exist');
+    // }
 
     // Execute the query
-    const features = new APIFeatures(Tour.find(), req.query).filter();
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .limitFields()
+      .paginatate();
     const tours = await features.query;
 
     // Send response
