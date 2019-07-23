2fd74a904466f96f8b1d46f5532e409043a0cb24
Paul Tobin <tobster111@gmail.com>
2019-07-23 16:02:26 +0100

    Implemented a catchAsync function to remove the repeative try/catch code

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -52,24 +52,21 @@ exports.getTour = async (req, res) => {
   }
 };
 
-exports.createTour = async (req, res) => {
-  try {
-    // const newTour = new Tour({})
-    // newTour.save()
-    const newTour = await Tour.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data sent'
-    });
-  }
+const catchAsync = fn => {
+  return (req, res, next) => {
+    fn(req, res, next).catch(err => next(err));
+  };
 };
+
+exports.createTour = catchAsync(async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour
+    }
+  });
+});
 
 exports.updateTour = async (req, res) => {
   try {
