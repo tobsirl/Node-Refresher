faf2ed48ec811c10434d9905ef968bc77f791690
Paul Tobin <tobster111@gmail.com>
2019-07-23 11:28:25 +0100

    Added global error handling middleware to catch all errors

--- natours/app.js
+++ natours/app.js
@@ -23,10 +23,28 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*', (res, req, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on the server`
+// 4) Middleware to catch bad requests
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on the server`
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on the server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
