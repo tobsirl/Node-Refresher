3fd392cc74465f2e612502936cc0c27e3f319e96
Paul Tobin <tobster111@gmail.com>
2019-07-24 13:27:00 +0100

    Added a function to deal with undhandled rejections in a grace way using server.close()

--- natours/server.js
+++ natours/server.js
@@ -9,6 +9,14 @@ dotenv.config();
 connectDatabase();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
