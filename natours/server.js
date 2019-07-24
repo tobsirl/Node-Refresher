f946a6b02e1638b9c1ae55d9c212a1e6614c8135
Paul Tobin <tobster111@gmail.com>
2019-07-24 15:19:30 +0100

    Added process.on(uncaughtException) to handle uncaught exceptions throughout our code

--- natours/server.js
+++ natours/server.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
 const app = require('./app');
 
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('Uncaught Exception! Shutting down...');
+  process.exit(1);
+});
+
 const connectDatabase = require('./database');
 
 dotenv.config();
