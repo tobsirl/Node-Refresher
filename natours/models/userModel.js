e6208b97ea70a2e4bccbb74e8d0400ad6a7f32a2
Paul Tobin <tobster111@gmail.com>
2019-07-25 10:25:31 +0100

    Added authController + /signup route

--- natours/models/userModel.js
+++ natours/models/userModel.js
@@ -4,12 +4,12 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please tell us your name!']
+    required: [true, 'Please tell us your name']
   },
 
   email: {
     type: String,
-    required: [true, 'Please provide your email!'],
+    required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email']
--- natours/routes/userRoutes.js
+++ natours/routes/userRoutes.js
@@ -6,8 +6,11 @@ const {
   updateUser,
   deleteUser
 } = require('./../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router
   .route('/')
