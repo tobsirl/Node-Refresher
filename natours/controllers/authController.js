d585171766e611cac959f471d94fa0e39d487487
Paul Tobin <tobster111@gmail.com>
2019-07-26 12:41:49 +0100

    Implementing changedPasswordAfter to check if the user has recently changed their password

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -70,11 +70,23 @@ exports.protect = catchAsync(async (req, res, next) => {
   }
   // 2) Verificaton Token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  console.log(decoded);
 
   // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError('The user belonging to this token no longer exists.', 401)
+    );
+  }
 
   // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401)
+    );
+  }
 
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
   next();
 });
