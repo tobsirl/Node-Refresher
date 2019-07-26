14bb3f2f773e5087b780ac6d94e7d4534289c3d5
Paul Tobin <tobster111@gmail.com>
2019-07-26 10:33:56 +0100

    Implementing the protect function that checks if a Bearer token has been sent in the header

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -51,3 +51,27 @@ exports.login = catchAsync(async (req, res, next) => {
     token
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting the token and check if it exists
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('Your are not logged in! Please log in to get access.', 401)
+    );
+  }
+  // 2) Verificaton Token
+
+  // 3) Check if user still exists
+
+  // 4) Check if user changed password after the token was issued
+
+  next();
+});
