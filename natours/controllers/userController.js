21f2eb3fedbafa7653e657fba36909fe9fa714da
Paul Tobin <tobster111@gmail.com>
2019-07-30 17:48:09 +0100

    Updating the user fields that need to be updated, using fiterObj functions

--- natours/controllers/userController.js
+++ natours/controllers/userController.js
@@ -2,6 +2,14 @@ const User = require('../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
 
@@ -15,7 +23,7 @@ exports.getAllUsers = catchAsync(async (req, res) => {
   });
 });
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if the user Posts password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -27,10 +35,16 @@ exports.updateMe = (req, res, next) => {
   }
 
   // 2) Update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
   res.status(200).json({
     status: 'success'
-  })
-};
+  });
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
