523cdb8a68c5aba8d989ce7314ac41ce842e83d0
Paul Tobin <tobster111@gmail.com>
2019-07-31 11:42:55 +0100

    Send back the cookie to the client with res.cookie() in createSendToken()

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -16,6 +16,14 @@ const signToken = id => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  res.cookie('jwt', token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    secure: true,
+    httpOnly: true
+  });
+
   res.status(statusCode).json({
     status: 'success',
     token,
