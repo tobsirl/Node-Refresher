56a35186f13570836668f377b0b0c0a83367254f
Paul Tobin <tobster111@gmail.com>
2019-07-31 12:33:21 +0100

    Installed express-mongo-sanitize and xss-clean and used both middlewares to increase security

--- natours/app.js
+++ natours/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +31,12 @@ const limiter = rateLimit({
 // 1) Global Middleware
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitiztaion against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Servering static files
 app.use(express.static(`${__dirname}/public`));
