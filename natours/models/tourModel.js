2fb5176dbb9dd120f95cf3170f601f8899d32f57
Paul Tobin <tobster111@gmail.com>
2019-08-05 16:44:41 +0100

    Added a compound index between price and ratingsAverage, added index for slug

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -114,7 +114,9 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
-tourSchema.index({ price: 1 });
+// tourSchema.index({ price: 1 });
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
 
 // Virtual properties
 tourSchema.virtual('durationWeeks').get(function() {
