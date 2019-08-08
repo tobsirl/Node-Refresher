865c7b5f28146e0476af0ec31abbea0f25adb2c6
Paul Tobin <tobster111@gmail.com>
2019-08-08 18:16:09 +0100

    Updated overview.pug with dynamic data

--- natours/controllers/viewsController.js
+++ natours/controllers/viewsController.js
@@ -1,8 +1,19 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('_overview', {
-    title: 'All Tours'
+const Tour = require('../models/tourModel');
+// const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+// const AppError = require('../utils/appError');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('_tour', {
