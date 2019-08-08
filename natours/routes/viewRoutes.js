876682cf994d662b1967842ac59ff8b1aacaacba
Paul Tobin <tobster111@gmail.com>
2019-08-08 16:22:10 +0100

    Added viewRoutes.js and mounted in app.js

--- natours/routes/viewRoutes.js
+++ natours/routes/viewRoutes.js
@@ -1,1 +1,24 @@
 const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas'
+  });
+});
+
+router.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+
+router.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker'
+  });
+});
+
+module.exports = router;
