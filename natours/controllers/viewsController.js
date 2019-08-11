8a6e788b7b50d3cc08a1d653e3ee7be9c0897e26
Paul Tobin <tobster111@gmail.com>
2019-08-11 10:26:48 +0100

    Added the pug code for finding the details on a tour

--- natours/controllers/viewsController.js
+++ natours/controllers/viewsController.js
@@ -26,7 +26,7 @@ exports.getTour = catchAsync(async (req, res) => {
   // 2) Build template
 
   // 3) Render template using data from 1
-  res.status(200).render('_tour', {
+  res.status(200).render('tour', {
     title: 'The Forest Hiker',
     tour
   });
