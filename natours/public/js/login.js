90f170df585ebe030cff3a69cf99eff56099ad48
Paul Tobin <tobster111@gmail.com>
2019-08-13 10:35:41 +0100

    After a user logs in successfully they are redirected to /

--- natours/public/js/login.js
+++ natours/public/js/login.js
@@ -1,25 +1,32 @@
 /* eslint-disable */
-import axios from 'axios';
+// import axios from 'axios';
 
 const login = async (email, password) => {
-  console.log(email, password);
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: 'http://localhost:3000/api/v1/users/login',
       data: {
         email,
         password
       }
     });
-    console.log(res);
+
+    if(res.data.status === 'success')  {
+      alert('Logged in successfully')
+      window.setTimeout(() => {
+        location.assign('/')
+      }, 1500)
+    }
+
+    // console.log(res);
   } catch (err) {
-    console.log(err);
+    alert(err.response.data.message);
   }
 };
 
 document.querySelector('.form').addEventListener('submit', e => {
-  e.preventDefault;
+  e.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   login(email, password);
--- natours/public/js/mapbox.js
+++ natours/public/js/mapbox.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-console.log('Hello from the client side');
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
 
