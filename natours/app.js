623cef33075c6af32ab22029d7d56b1a9cc32995
Paul Tobin <tobster111@gmail.com>
2019-08-13 08:16:17 +0100

    Created the isLoggedIn function for the frontend

--- natours/app.js
+++ natours/app.js
@@ -9,6 +9,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 
+const cookieParser = require('cookie-parser');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -44,6 +45,7 @@ const limiter = rateLimit({
 // 1) Global Middleware
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitiztaion against NoSQL query injection
 app.use(mongoSanitize());
@@ -67,6 +69,7 @@ app.use(
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
--- natours/public/js/login.js
+++ natours/public/js/login.js
@@ -1,7 +1,21 @@
 /* eslint-disable */
+import axios from 'axios';
 
-const login = (email, password) => {
-  
+const login = async (email, password) => {
+  console.log(email, password);
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password
+      }
+    });
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 document.querySelector('.form').addEventListener('submit', e => {
